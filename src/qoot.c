135500ac8fbd058d24b19e37036fe9a3721e7ff1
Quaylyn Rimer <quaylynrimer11@gmail.com>
2026-03-01 22:00:47 -0700

    Add copyright and MIT license headers to all source files
    Copyright (c) 2026 Quaylyn Rimer - full MIT license text in every
    source file header: qoot.c, syscalls.h, string.h, auth.h, and LICENSE.

--- src/qoot.c
+++ src/qoot.c
@@ -1,6 +1,28 @@
 /*
  * qoot - A sudo alternative with ZERO glibc dependency
  *
+ * Copyright (c) 2026 Quaylyn Rimer. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * ---
+ *
  * This program provides privilege escalation using only raw Linux
  * syscalls. No glibc, no musl, no libc of any kind. Pure syscalls.
  *
@@ -21,7 +43,7 @@
  * The binary MUST be setuid root to function.
  *
  * Configuration: /etc/qoot.conf
- *   # Allow specific user
+ *   # Allow specific user (password required)
  *   username
  *   # Allow user without password
  *   username NOPASSWD
@@ -29,8 +51,6 @@
  *   ALL
  *   # Allow all users without password
  *   ALL NOPASSWD
- *
- * Copyright (c) 2026 - MIT License
  */
 
 #include "syscalls.h"
